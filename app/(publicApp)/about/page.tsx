6cfe957c0c2c8aa4fb6497e3816b66aa6e5abedb
Jonasoft <jonathan.monroy@live.com>
2024-03-21 21:22:01 -0300

    first comit

---
+++ app/(publicApp)/about/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "about page",
+  description: "about",
+  keywords: ['about page', 'info'],
+};
+
+const AboutPage = () => {
+  return (
+    <div>AboutPage</div>
+  )
+}
+
+export default AboutPage
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center p-24">
+      <span>Hello worldd</span>
+    </main>
+  );
+}
